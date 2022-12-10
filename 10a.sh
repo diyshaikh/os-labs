479cda98663a6d38f9321095ce85d5db4f0f7f7d
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-12-10 23:32:07 +0530

    check prime

--- 10a.sh
+++ 10a.sh
@@ -3,8 +3,8 @@ echo "enter a number"
 read n
 for ((i = 2;i<=$n/2;i++))
 do
-    ans = $((n%i))
-    if [$ans -eq 0 ]
+    ans=$((n%i))
+    if [ $ans -eq 0 ]
     then
         echo "$n is not a prime number."
         exit 0
