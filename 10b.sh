075946c9c0e014358b665f111567cd1528625140
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-12-10 17:01:16 +0530

    reversing number  without recursion

--- 10b.sh
+++ 10b.sh
@@ -6,8 +6,7 @@ temp=$n
 while (($temp>0))
 do
     k=$((temp % 10))
-    num=$((num * 10))
-    num=$((num + k))
+    num=$((num * 10 +k))
     temp=$((temp / 10))
 done
 echo "$num is reverse of $n."
